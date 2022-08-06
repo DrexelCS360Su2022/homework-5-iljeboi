deb0d587088a3b332a58eceb2271905d5aea3c1f
iljeboi <50242003+iljeboi@users.noreply.github.com>
2022-08-06 17:30:43 -0400

    Add files via upload

--- HaskellIntro.hs
+++ HaskellIntro.hs
@@ -1,8 +1,18 @@
 {-# OPTIONS_GHC -fwarn-tabs #-}
+{-# OPTIONS_GHC -Wno-deferred-out-of-scope-variables #-}
+{-# OPTIONS_GHC -Wno-overlapping-patterns #-}
+{-# LANGUAGE BlockArguments #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Redundant where" #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
 
 module HaskellIntro where
 
 import Set
+import Distribution.Simple.Utils (xargs)
 
 -- Load this file into GHCi (say, with `ghci HaskellIntro.hs`) and type
 -- `isThisWorking` at the prompt. GHCi will tell you whether it's working!
@@ -15,41 +25,76 @@ isThisWorking = "Yes"
 --
 
 lastDigit :: Integer -> Integer
-lastDigit = error "lastDigit not yet defined"
+lastDigit = if x < 10 then x else mod x 10
 
 dropLastDigit :: Integer -> Integer
-dropLastDigit = error "dropLastDigit not yet defined"
+dropLastDigit x = if x < 10 then 0 else div x 10
 
 toDigits :: Integer -> [Integer]
-toDigits = error "toDigits not yet defined"
+toDigits x = if dropLastDigit x == 0 then [] else map lastDigit (toDigits (dropLastDigit x))
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = error "doubleEveryOther not yet defined"
+doubleEveryOther x = doubleHelp (reverse x)
+
+doubleHelp :: [Integer] -> [Integer]
+doubleHelp [] = []
+doubleHelp (x:xs) = x : (2 * head xs) : doubleHelp (tail xs)
 
 sumDigits :: [Integer] -> Integer
-sumDigits = error "sumDigits not yet defined"
+sumDigits [] = 0
+sumDigits (x:xs) = sum (toDigits x) + sumDigits xs
 
 validate :: Integer -> Bool
-validate = error "validate not yet defined"
-
+validate 0 = False
+validate x = sumDigits (toDigits x) <= 4012888888881881
 --
 -- Problem 2
 --
-
-pow :: (a -> a) -> Int -> a -> a
-pow = error "pow not yet defined"
+pow2 :: (Eq t1, Num t1) => t2 -> t3 -> t1 -> t2
+pow2 f x n = pow2 f x n
+    where
+        pow2 f x 1 = f
+        pow2 f x n = pow2 f x (n-1)
 
 g :: Integer -> Integer
-g = error "g not yet defined"
+g x = g x
+    where
+        g 0 = 0
+        g x = x - pow2 (g (x-1) 2)
 
 h :: Integer -> Integer
-h = error "h not yet defined"
+
+h x = h x
+    where
+        h 0 = 0
+        h x = x - pow2 (h (x-1) 3)
 
 d :: Int -> Integer -> Integer
-d = error "d not yet defined"
+d x = d x
+    where
+        d 0 = 0
+        d x = x - pow2 (d (x-1) 1)
 
 --
 -- Problem 3
 --
+--powerSet :: Set a -> Set(Set a)
+--powerSet x = insert empty (step isEmpty pxs) where
+--  step x pxs = insert (singleton pxs) 
+--                insert (mapSet split pxs )  q\
 
-powerSet = error "powerSet not yet defined"
+
+
+square :: Int -> Int
+square x = x*x
+
+
+
+powerSet :: Set a -> Set (Set a)
+powerSet isEmpty = empty
+powerSet x = powerSHelp (split x)
+
+powerSHelp :: (a, Set a) -> Set (Set a)
+powerSHelp (x, xs) = mapSet (insert x) (powerSet xs)  `union` powerSet xs
+
+
--- README.md
+++ README.md
@@ -5,13 +5,13 @@ Name:
 Drexel Username:  
 
 Put anything else you want us to know here.
-
+I'm sorry for the late submission. This term has been a struggle 
 ## Problem 4: Homework statistics
 
 Please let us know how much time you spent on each problem. You may enter time using any format described [here](https://github.com/wroberts/pytimeparse). Please put your time estimates *on the same line* as the problem label, not on the following line.
 
-Problem 1:  
+Problem 1:  3h30m
 
-Problem 2:  
+Problem 2:  4h
 
-Problem 3:  
+Problem 3:  9h
